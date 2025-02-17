3bb7cb81ebb302e8e6124490c0edd5e2df8d897f
Sahilkadam30 <sahilkadam30303@gmail.com>
2025-02-17 23:40:24 +0530

    1st complete

---
+++ arrayrotate.java
@@ -0,0 +1,11 @@
+public class arrayrotate {
+    public static void main(String[] args){
+        int[] arr ={1,2,3,4,5,6,7};
+        int start= arr[0];
+        int end=arr.length-1;
+        int temp[]=new int[end];
+        
+    }
+    
+   
+}
