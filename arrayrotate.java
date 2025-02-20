1f74c434735d7274ac1ff342b2e78b25709dfaa4
Sahilkadam30 <sahilkadam30303@gmail.com>
2025-02-20 18:15:51 +0530

    array rotate

--- arrayrotate.java
+++ arrayrotate.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+
 public class arrayrotate {
     public static void main(String[] args){
-        int[] arr ={1,2,3,4,5,6,7};
-        int start= arr[0];
-        int end=arr.length-1;
-        int temp[]=new int[end];
+        int[] arr={1,2,3,4,5,6,7,8};
+        int k=1;
+        reverse(arr,0,arr.length-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,arr.length-1);
+        System.out.println(Arrays.toString(arr));
         
     }
-    
-   
+
+    static void reverse(int[] arr , int start,int end){
+        int temp;
+        while(start < end){
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }  
 }
